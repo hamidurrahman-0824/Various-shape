477aea29859a33d4eedf6a60a77efc79bd7c7f09
Hamidur Rahman Majed <2022334042@student.sust.edu>
2025-09-04 00:23:19 +0600

    Update second_week.py

--- second_week.py
+++ second_week.py
@@ -37,3 +37,35 @@ for i in range(1):
         print("*",end= "")
 for j in range(5):
     print(" " * (4 - j) + "*")
+for i in range(1,6):
+    for j in range(1,6):
+        if i == 1 or i == 5 or j == 1 or j == 5:
+            print("*", end="")
+        else:
+            print(" ", end= "")
+    print()
+rows = 5
+
+for i in range(rows):
+    for j in range(rows):
+        if i == 0 or i == rows - 1 or j == 0 or j == rows - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+row= 5
+for i in range(1,6):
+    if i==3:
+            print("* *")
+    elif i== 4:
+        print("*  *")
+    else:
+       print("*"*i)
+row= 5
+for i in range(1,row+1):
+    if i == 1:
+        print("*")
+    elif i == row:
+        print("*"*row)
+    else:
+        print("*"+" "*(i-2)+"*")
